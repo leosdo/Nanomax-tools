2a1272e453d1f3b1938fded7508bdf6a8ab3d391
lheuzim <158077273+lheuzim@users.noreply.github.com>
2024-11-04 22:11:39 +0100

    references

--- Home.py
+++ Home.py
@@ -21,3 +21,11 @@ st.markdown(
     [![License: MIT](https://cdn.prod.website-files.com/5e0f1144930a8bc8aace526c/65dd9eb5aaca434fac4f1c34_License-MIT-blue.svg)](/LICENSE)
 """
 )
+st.divider()
+st.markdown(
+    """
+    The calculation routines primarily utilize the following libraries:
+    - XrayDB: https://github.com/xraypy/xraydb/
+    - DansDiffraction: https://github.com/DanPorter/Dans_Diffraction
+"""
+ )
