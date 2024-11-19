b661ca1eb7e83703ac84fae0f0ca025a62aff22c
lheuzim <78479683+leosdo@users.noreply.github.com>
2024-11-19 12:58:23 +0100

    x-ray emission lines added

---
+++ libs/xray_emissison.py
@@ -0,0 +1,31 @@
+import xraydb
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plotxraylines(atom_list):
+    fig, ax = plt.subplots(np.shape(atom_list)[0], 1, figsize = (6 , 10), dpi = 600, sharex = True, sharey = True)
+    for i, element in enumerate(atom_list):
+        edge = xraydb.xray_edges(element)
+        #
+        for edge_key, j in edge.items():
+            #colors = cmap(np.linspace(0, 1, np.shape(energy_key)))
+            for key, value in xraydb.xray_lines(element, edge_key).items(): 
+                energy = value[0]/1000
+                if energy <= 25:
+                    #print(element, key, energy)
+                    intensity = value[1]
+                    #level = key + "(" + value[2] + "-" + value[3] +")"
+                    level = key
+                    ax[i].bar(energy, intensity, width = 0.08, label = level)
+                    ax[i].grid(lw = 0.3)
+                    #ax[i].text(energy - 0.2,intensity + 0.02, level)
+        ax[i].legend(ncols = 4, title = f'{element} edges', fontsize=8, loc = 'upper left', bbox_to_anchor = (1.05, 1.1), markerscale = 0.1, frameon = False)
+    ax[0].set_xlim(0, 26)
+    #ax[0].set_ylim(0, 1e0)
+    ax[0].set_yscale("log")
+    ax[0].set_ylim(1e-5, 1)
+    ax[i].set_xlabel("Energy [keV]")
+    ax[i].set_xticks(np.arange(0, 27, 2))
+    ax[0].set_title("X-ray emission lines")
+    plt.show()
+    return fig
---
+++ pages/4_xray_emission_lines.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from libs import xray_emissison
+
+
+st.title ("X ray emission lines")
+st.markdown(r"""
+Type below the element you are interest to check the if the emission lines are in the range
+of X-ray energy you are targeting the sample.  
+
+NB: Oxigen and Argon will always be ploted alongwith the selected elements.
+
+""")
+
+st.subheader("Select atoms", divider=True)
+col_f1, col_f2, col_f3, col_f4 = st.columns(4)
+
+#col_f5 = st.columns(1)[0]
+with st.form(key="form_combined"):
+    placeholder= "Empty"
+    with col_f1:
+        el_1 = st.text_input("Element 1", placeholder= placeholder)
+    with col_f2:
+        el_2 = st.text_input("Element 2", placeholder= placeholder)
+    with col_f3:
+        el_3 = st.text_input("Element 3", placeholder= placeholder)
+    with col_f4:
+        el_4 = st.text_input("Element 4", placeholder= placeholder)
+    submitted = st.form_submit_button("Run", use_container_width = True)
+ # After the form is submitted, display the inputs
+if submitted:
+    atom_list = [el_1, el_2, el_3, el_4]
+    atom_list = ' '.join(atom_list).split()
+    for i in atom_list:
+        if not i.isalpha(): #check if they are letters or not
+            st.warning("Please provide only letters for elements.")
+            atom_list.remove(i) 
+            #atoms_list = ' '.join(atom_list).split()
+    atom_list.extend(['Ar', 'O'])
+    fig = xray_emissison.plotxraylines(atom_list)
+    st.pyplot(fig)
